2a49b499391800b734798588d84b16efd5111338
iampoojitha <162096747+iampoojitha@users.noreply.github.com>
2025-04-19 12:05:56 +0530

    Add files via upload

---
+++ Translation-Service/src/main/java/Data/Translation_Service/TranslationServiceApplication.java
@@ -0,0 +1,13 @@
+package Data.Translation_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationServiceApplication.class, args);
+	}
+
+}
