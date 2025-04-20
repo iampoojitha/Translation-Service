ec5f2944ed0557603bef178181de6238e4e4c7e8
Poojitha <poojithamsvce@gmail.com>
2025-04-20 16:00:44 +0530

    implemented redis cache to fetch the translations.

--- Translation-Service/src/main/java/Data/Translation_Service/TranslationServiceApplication.java
+++ Translation-Service/src/main/java/Data/Translation_Service/TranslationServiceApplication.java
@@ -2,8 +2,10 @@ package Data.Translation_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TranslationServiceApplication {
 
 	public static void main(String[] args) {
